6c62ae8b4acd909712833919b81ab2ed28a05069
kawanmelo <kawangcmelo@gmail.com>
2023-12-31 11:40:06 -0300

    adding comments to endpoints files

--- Endpoints/Get/UserGetById.cs
+++ Endpoints/Get/UserGetById.cs
@@ -4,25 +4,25 @@ using Microsoft.AspNetCore.Mvc;
 
 /*
 Este arquivo contém a lógica para lidar com a recuperação de usuários por meio de uma solicitação Get.
-O endpoint UserGetByName interage com o banco de dados para recuperação do usuário.
+O endpoint UserGetById interage com o banco de dados para recuperação do usuário.
 */
 namespace MeloSolution.authenticationAPI.Endpoints{
-    public class UserGetByName{
+    public class UserGetById{
         private static  IConsumeObject consumeObject;
-        public static string TemplateUserByName => "/User/GetName/{name}";
-        public static string[] MetodoUserByName => new string[] {HttpMethod.Get.ToString()};
-        public static Delegate FuncUserByName => ActionUserByName;
+        public static string TemplateUserById => "/User/GetId/{Id:int}";
+        public static string[] MetodoUserById => new string[] {HttpMethod.Get.ToString()};
+        public static Delegate FuncUserById => ActionUserById;
         /*
-        Busca um usuário no banco de dados por meio de uma solicitação GET usando o nome como critério de pesquisa.
+        Busca um usuário no banco de dados por meio de uma solicitação GET usando o Id como critério de pesquisa.
         - Instancia o objeto responsável por consumir informações do banco de dados.
-        - Executa a consulta no banco de dados para recuperar um usuário com o nome fornecido.
+        - Executa a consulta no banco de dados para recuperar um usuário com o Id fornecido.
         - Retorna um resultado Ok com os detalhes do usuário se encontrado, ou NotFound se não encontrado.
         */
-        public static IResult ActionUserByName([FromRoute] string name){
+        public static IResult ActionUserById([FromRoute] int id){
             try{
                 consumeObject = new ConsumeUser();
                 // Executa a consulta no banco de dados para recuperar um usuário com o nome fornecido.
-                var userFound = consumeObject.ConsumeUniqueObject($"SELECT * FROM dbo.UserData WHERE Name LIKE '{name}'");
+                var userFound = consumeObject.ConsumeUniqueObject($"SELECT * FROM dbo.UserData WHERE UserId LIKE '{id}'");
                 if(userFound != null){
                     // Retorna um resultado Ok (200 OK) com os detalhes do usuário se encontrado.
                     return Results.Ok(userFound);
